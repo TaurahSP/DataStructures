02b83e2c82034c330f898bc5f6f8513067122c9d
Keshav Taurah <keshavtaurah@yahoo.com>
2020-07-12 16:52:38 -0700

    Array implementation completed

---
+++ DataStructures/DataStructures/Arrays/Arrays.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+/// <summary>
+/// This class implements an array
+/// </summary>
+public class ArrayImplementation
+{
+    public int length;
+    public object[] data;
+
+    public ArrayImplementation()
+    {
+        this.length = 0;
+        this.data = new object[1];
+    }
+
+    public object get(int item) { return this.data[item]; }
+
+    public Object[] push(Object item)
+    {
+        if(this.data.Length == this.length)
+        {
+            //copy initial array to a temporary array of object
+            Object[] temp = new Object[this.length];
+            Array.Copy(this.data, temp, length);
+
+            //Create a new array obejct with a bigger size to be able to add the new element later.
+            this.data = new Object[length + 1];
+            Array.Copy(temp, this.data, length);
+        }
+        this.data[this.length] = item;
+        this.length++;
+        return this.data;
+    }
+
+    private void shiftItems(int item)
+    {
+        for(int i = item; i < length - 1; i++)
+        {
+            this.data[i] = this.data[i + 1];
+        }
+
+        //now we need to remove that last item 
+        this.data[length - 1] = null;
+
+        this.length--;
+    }
+
+    public void delete(int item) { this.shiftItems(item); }
+
+    public void pop()
+    {
+        this.data[this.length - 1] = null;
+        this.length--;
+    }
+}
+
---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        #region Array
+            //var Array = new ArrayImplementation();
+            //Array.push("hi");
+            //Array.push("you");
+            //Array.push("!");
+            //Array.delete(2);
+
+            //for (int i = 0; i < Array.length; i++)
+            //{
+            //    Console.WriteLine(Array.get(i));
+            //}
+            //for (int i = 0; i < Array.length; i++)
+            //{
+            //    Console.WriteLine(Array.get(i));
+            //}
+            //Array.pop();
+            //Console.ReadLine(); 
+            #endregion
+        }
+    }
+}
