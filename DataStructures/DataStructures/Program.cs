15e95f1c019231146ca3c5ca743dc04094977388
Keshav Taurah <keshavtaurah@yahoo.com>
2020-07-13 20:05:14 -0700

    merge two sorted arrays solution added

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -1,9 +1,6 @@
 ﻿using DataStructures.Arrays;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructures
 {
@@ -30,10 +27,27 @@ namespace DataStructures
             //Console.ReadLine(); 
             #endregion
 
-        #region ReverseString
+            #region ReverseString
             //ReverseString reverseString = new ReverseString();
             //Console.WriteLine(reverseString.reverse("My  name  is keshav"));
             //Console.ReadLine(); 
+            #endregion
+
+            #region MergeArrays
+            //MergeArrays mergeArrays = new MergeArrays();
+            //int[] a = new int[] { 1, 4, 8, 20 };
+            //int n1 = a.Length;
+
+            //int[] b = new int[] { 17, 21 };
+            //int n2 = b.Length;
+
+            //int[] arr3 = new int[n1 + n2];
+
+            //mergeArrays.mergesortedarray(a, b, n1, n2, arr3);
+
+            //Console.Write("Array after merging\n");
+            //for (int i = 0; i < n1 + n2; i++)
+            //    Console.Write(arr3[i] + " "); 
             #endregion
         }
     }
