91a651b6ce893d757986a00f8717c376b3c1ce88
Keshav Taurah <keshavtaurah@yahoo.com>
2020-07-18 21:06:12 -0700

    queue using linkedlist implementation added

---
+++ DataStructures/DataStructures/Queue/Queue_Using_LinkedList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Queue
+{
+    public class Queue_Using_LinkedList
+    {
+        public Node first;
+        public Node last;
+        public int length;
+
+        public Queue_Using_LinkedList()
+        {
+            this.first = null;
+            this.last = null;
+            this.length = 0;
+        }
+
+        public int peek()
+        {
+            if(this.length > 0)
+            {
+                return this.first.value;
+            }
+            return -111111;  //returining large negative value if length is 0.
+        }
+
+        public void enqueue(int value)
+        {
+            Node newNode = new Node(value);
+            if(this.length == 0)
+            {
+                this.first = newNode;
+                this.last = newNode;
+            } else
+            {
+                this.last.next = newNode;
+                this.last = newNode;
+            }
+            this.length++;
+        }
+
+        public int dequeue()
+        {
+            if(this.first == null)
+            {
+                return -1111111;
+            }
+            if(this.length == 0)
+            {
+                this.last = null;
+            }
+            Node holdingPointer = this.first;
+            this.first = this.first.next;
+
+            this.length--;
+
+            return holdingPointer.value;
+        }
+
+        public void printQueue()
+        {
+            if(first == null)
+            {
+                return;
+            }
+            Node currentNode = first;
+            Console.Write(currentNode.value);
+            currentNode = currentNode.next;
+            while(currentNode != null)
+            {
+                Console.Write("-->" + currentNode.value);
+                currentNode = currentNode.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
