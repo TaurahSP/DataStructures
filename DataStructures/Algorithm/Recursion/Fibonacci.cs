50c5ea5ac54529418cf8a35842310b655e927d06
Keshav Taurah <keshavtaurah@yahoo.com>
2020-07-22 22:35:15 -0700

    added fibonacci function signatures

--- DataStructures/Algorithm/Recursion/Fibonacci.cs
+++ DataStructures/Algorithm/Recursion/Fibonacci.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithm.Recursion
 {
+    /// <summary>
+    /// Given a number N return the index value of the Fibonacci sequence,where the sequence is:
+    /// 0,1,1,2,3,5,8,13,21,34,55,89,144... the pattern of the sequence is that each value is the sum of the 2 previous
+    /// values,that means taht N=5 =>2+3
+    /// </summary>
     public class Fibonacci
     {
+        public int FibonnaciRecursive(int number) { return number; }
+
+        public int FibonacciIterative(int number) { return number; }
     }
 }
