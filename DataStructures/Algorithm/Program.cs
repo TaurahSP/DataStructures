15207271d9df24d97d047306a339710c0b095a81
Keshav Taurah <keshavtaurah@yahoo.com>
2020-08-03 19:43:27 -0700

    BFS completed

--- DataStructures/Algorithm/Program.cs
+++ DataStructures/Algorithm/Program.cs
@@ -1,10 +1,5 @@
-﻿using Algorithm.Recursion;
-using Algorithm.Sorting;
+﻿using Algorithm.Traversal;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithm
 {
@@ -12,7 +7,22 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
+            BinarySearchTree tree = new BinarySearchTree();
+            tree.insert(9);
+            tree.insert(4);
+            tree.insert(6);
+            tree.insert(20);
+            tree.insert(170);
+            tree.insert(15);
+            tree.insert(1);
 
+            BreadthFirstSearch bfs = new BreadthFirstSearch();
+            var result = bfs.breathFirstSearch(tree.root);
+            foreach(var item in result)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
--- DataStructures/Algorithm/Traversal/BreadthFirstSearch.cs
+++ DataStructures/Algorithm/Traversal/BreadthFirstSearch.cs
@@ -1,6 +1,109 @@
-﻿namespace Algorithm.Traversal
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace Algorithm.Traversal
 {
     public class BreadthFirstSearch
     {
+        //Iterative
+        public  List<int> breathFirstSearch(Node currentNode)
+        {
+            List<int> list = new List<int>();
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(currentNode);
+
+            while(queue.Count > 0)
+            {
+                currentNode = queue.Dequeue();
+                list.Add(currentNode.value);
+                if(currentNode.left != null)
+                {
+                    queue.Enqueue(currentNode.left);
+                }
+
+                if(currentNode.right != null)
+                {
+                    queue.Enqueue(currentNode.right);
+                }
+            }
+            return list;
+        }
+
+        //Recurcive
+        public  List<int> breathFirstSearchR(Queue<Node> queue, List<int> list)
+        {
+            if(queue.Count == 0)
+            {
+                return list;
+            }
+
+            var currentNode = queue.Dequeue();
+            list.Add(currentNode.value);
+            if(currentNode.left != null)
+            {
+                queue.Enqueue(currentNode.left);
+            }
+
+            if(currentNode.right != null)
+            {
+                queue.Enqueue(currentNode.right);
+            }
+
+            return breathFirstSearchR(queue, list);
+        }
+    }
+
+    public class Node
+    {
+        public Node left { get; set; }
+
+        public Node right { get; set; }
+
+        public int value { get; set; }
+
+        public Node(int value)
+        {
+            this.left = null;
+            this.right = null;
+            this.value = value;
+        }
+    }
+
+    class BinarySearchTree
+    {
+        public Node root;
+        public BinarySearchTree() { this.root = null; }
+
+        public void insert(int value)
+        {
+            Node newNode = new Node(value);
+            if(this.root == null)
+            {
+                this.root = newNode;
+                return;
+            }
+
+            Node currentNode = this.root;
+            while(true)
+            {
+                if(currentNode.value > value)
+                {
+                    if(currentNode.left == null)
+                    {
+                        currentNode.left = new Node(value);
+                        return;
+                    }
+                    currentNode = currentNode.left;
+                } else
+                {
+                    if(currentNode.right == null)
+                    {
+                        currentNode.right = new Node(value);
+                        return;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
     }
 }
