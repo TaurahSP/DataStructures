0245be0eec3338f8794b307f5aea30b6eb6c75a3
Keshav Taurah <keshavtaurah@yahoo.com>
2020-08-07 20:45:14 -0700

    DP-fibonnaci

--- DataStructures/Algorithm/Dynamic Programming/Dynamic.cs
+++ DataStructures/Algorithm/Dynamic Programming/Dynamic.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Algorithm.Dynamic_Programming
 {
-    class Dynamic
+    public class Dynamic
     {
+        private static Dictionary<int, int> cache = new Dictionary<int, int>();
+
+        public static int fibonacciMaster(int n)
+        {
+            if(cache.ContainsKey(n))
+            {
+                return cache[n];
+            }
+            if(n < 2)
+            {
+                return n;
+            }
+            cache.Add(n, fibonacciMaster(n - 1) + fibonacciMaster(n - 2));
+            return cache[n];
+        }
     }
 }
